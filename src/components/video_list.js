eaf73b288388225799c65b8c393f8ac478eec5b0
asonj <ajohnson1993@gmail.com>
2018-02-04 10:09:30 -0800

    functioning search

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
+// functional component, no state
 const VideoList = (props) => {
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem
+      key={video.etag}
+      video={video}
+      onVideoSelect = {props.onVideoSelect} />
+  })
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 }
